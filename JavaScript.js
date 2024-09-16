339a04813f918d147063e0b4d4aa22512eef8e64
shi-km <julietwanjiru333@gmail.com>
2024-09-16 22:13:16 +0300

    initial commit

---
+++ JavaScript.js
@@ -0,0 +1,48 @@
+console. log("     ");
+console. log("Hello world");
+console. log("arise and shine ");
+
+
+
+
+
+
+
+
+
+<h2>GET IN TOUCH</h2>
+<div class="list">
+   <ul>
+       <li>Nairobi,Kenya</li>
+       <li>+254726117986/ +254705182141</li>
+       <li>info@timazi.org</li>
+       <li>P. O. Box 1804-00902 Kikuyu</li>
+       
+   </ul>
+
+
+
+   <div class="footer-col"></div>
+                <h2>RESOURCES</h2>
+                <div class="list">
+                   <ul>
+                       <li><a></a>Podcasts</li>
+                       <li><a></a>videos</li>
+                       <li><a></a>Articles</li>
+                       
+                   </ul>
+   
+            </div>
+
+
+            <div class="footer-col">
+                <h2>PROGRAMS</h2>
+                <div class="list">
+                   <ul>
+                       <li><a></a>Timazi Magazine</li>
+                       <li><a></a>Readership Challenge</li>
+                       <li><a></a>Bible Reading program</li>
+                       <li><a></a>Mwalimu Bora</li>
+                       
+                   </ul> 
+                   </div>
